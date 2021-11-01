ca67c158253d2756ee3ad9c2ebb45e96bcaffffd
WhoCanThisBe <wen.hhp@gmail.com>
2021-11-01 18:08:58 +0100

    added dropdown

--- src/Navigation/NavigationBar.tsx
+++ src/Navigation/NavigationBar.tsx
@@ -1,21 +1,40 @@
-import {Nav,Navbar} from "react-bootstrap";
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 import logo from '../Images/logo.svg'
 import {Link} from "react-router-dom";
 
 const NavigationBar = () =>{
 
     return(
-        <div>
-            <Navbar bg="myGrey" variant="dark">
+        <div >
+            <Navbar bg="myGrey menuOpt" variant="dark">
                 <Navbar.Brand>
-                    <img src={logo}/>
+                    <img src={logo} width="40px" height="40px"/>
                     Logo
                 </Navbar.Brand>
-
                 <Nav>
-                    <Nav.Link as={Link} to={"/female-home"}>Female</Nav.Link>
-                    <Nav.Link as={Link} to={"/male-home"}>Male</Nav.Link>
-                    <Nav.Link as={Link} to={"/unisex-home"}>Unisex</Nav.Link>
+                    <Nav.Link as={Link} to={"/female-home"}> Female
+                        <NavDropdown title="">
+                            <NavDropdown.Item>Klær</NavDropdown.Item>
+                            <NavDropdown.Item>Sko</NavDropdown.Item>
+                            <NavDropdown.Item>Accesories</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav.Link>
+                    <Nav.Link as={Link} to={"/male-home"}>Male
+                        <NavDropdown title="" >
+                            <NavDropdown.Item>Klær</NavDropdown.Item>
+                            <NavDropdown.Item>Sko</NavDropdown.Item>
+                            <NavDropdown.Item>Accesories</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav.Link>
+                    <Nav.Link as={Link} to={"/unisex-home"}>Unisex
+                        <NavDropdown title="">
+                            <NavDropdown.Item>Klær</NavDropdown.Item>
+                            <NavDropdown.Item>Sko</NavDropdown.Item>
+                            <NavDropdown.Item>Accesories</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav.Link>
+
+
                 </Nav>
             </Navbar>
         </div>
