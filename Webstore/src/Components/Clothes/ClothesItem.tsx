f214be1f7511f64df3a538bad9891c29aa23e626
CodeFreeBugs <56070299+CodeFreeBugs@users.noreply.github.com>
2021-11-04 13:25:43 +0100

    #12 Add "dependencyArray" to useEffect in `ClothesContext.tsx` to avoid "spamming the backend server with requests".

--- Webstore/src/Components/Clothes/ClothesItem.tsx
+++ Webstore/src/Components/Clothes/ClothesItem.tsx
@@ -1,27 +1,30 @@
-import React, {FC} from "react";
-import {Button, Card} from "react-bootstrap";
-import {useHistory} from "react-router-dom";
-import {IProduct} from "../../Interfaces/IProduct";
+import React, { FC } from "react";
+import { Button, Card } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { IProduct } from "../../Interfaces/IProduct";
 
 type Props = {
-    garment: IProduct;
+  garment: IProduct;
 };
 
-const ClothesItem: FC<Props> = ({garment}) => {
-    const history = useHistory();
+const ClothesItem: FC<Props> = ({ garment }) => {
+  const history = useHistory();
 
-    return (
-        <Card className={"w-50"}>
-            <Card.Img variant={"top"} src={`https://localhost:5001/images/${garment.image}`}/>
-            <Card.Body>
-                <Card.Text>{garment.brandName}</Card.Text>
-                <Card.Title>{garment.clothingName}</Card.Title>
-                <Button variant={"primary"} onClick={() => history.push("/cart")}>
-                    Add to cart
-                </Button>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card className={"w-50"}>
+      <Card.Img
+        variant={"top"}
+        src={`https://localhost:5001/images/${garment.image}`}
+      />
+      <Card.Body>
+        <Card.Text>{garment.brandName}</Card.Text>
+        <Card.Title>{garment.clothingName}</Card.Title>
+        <Button variant={"primary"} onClick={() => history.push("/cart")}>
+          Add to cart
+        </Button>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default ClothesItem;
--- Webstore/src/Contexts/ClothesContext.tsx
+++ Webstore/src/Contexts/ClothesContext.tsx
@@ -1,8 +1,8 @@
-import React, {FC, useEffect, useState} from "react";
-import {ClothesContextType} from "../types/ClothesContextType";
-import {IProduct} from "../Interfaces/IProduct";
+import React, { FC, useEffect, useState } from "react";
+import { ClothesContextType } from "../types/ClothesContextType";
+import { IProduct } from "../Interfaces/IProduct";
 import testLogo from "../Images/logo512.png";
-import {ClothesService} from "../Services/ClothesService";
+import { ClothesService } from "../Services/ClothesService";
 
 type Props = {
   children: React.ReactNode;
@@ -14,48 +14,48 @@ export const ClothesContext = React.createContext<ClothesContextType | null>(
 
 // Disse klærne blir lagt i databasen, men jeg lar de ligge her, i tilfelle noe må testes raskt,
 // eller om man ikke skulle ha tilgang til databasen. Kommenter ut getClothes() fra useEffekt for å bruke disse
-const ClothesProvider: FC<Props> = ({children}) => {
-    const [clothes, setClothes] = useState<IProduct[]>([
-        {
-            brandName: "Armani",
-            clothingName: "Blå Jakke",
-            categoryType: "jakke",
-            size: "large",
-            stock: 5,
-            color: "blue",
-            gender: "male",
-            image: testLogo
-        },
-        {
-            brandName: "Gucci",
-            clothingName: "Brun Bag",
-            categoryType: "genser",
-            size: "small",
-            stock: 10,
-            color: "brown",
-            gender: "unisex",
-            image: testLogo
-        },
-        {
-            brandName: "H&M",
-            clothingName: "Gul Bukse",
-            categoryType: "bukse",
-            size: "medium",
-            stock: 10,
-            color: "brown",
-            gender: "female",
-            image: testLogo
-        },
-    ]);
+const ClothesProvider: FC<Props> = ({ children }) => {
+  const [clothes, setClothes] = useState<IProduct[]>([
+    {
+      brandName: "Armani",
+      clothingName: "Blå Jakke",
+      categoryType: "jakke",
+      size: "large",
+      stock: 5,
+      color: "blue",
+      gender: "male",
+      image: testLogo,
+    },
+    {
+      brandName: "Gucci",
+      clothingName: "Brun Bag",
+      categoryType: "genser",
+      size: "small",
+      stock: 10,
+      color: "brown",
+      gender: "unisex",
+      image: testLogo,
+    },
+    {
+      brandName: "H&M",
+      clothingName: "Gul Bukse",
+      categoryType: "bukse",
+      size: "medium",
+      stock: 10,
+      color: "brown",
+      gender: "female",
+      image: testLogo,
+    },
+  ]);
 
-    useEffect( () => {
-        getClothes();
-    })
+  useEffect(() => {
+    getClothes();
+  }, []);
 
-    const getClothes = async () => {
-        const _clothes = await ClothesService.getAll();
-        setClothes( _clothes );
-    }
+  const getClothes = async () => {
+    const _clothes = await ClothesService.getAll();
+    setClothes(_clothes);
+  };
 
   const fetchProductsByGender = (gender: IProduct["gender"]) => {
     return clothes.filter((garment) => garment.gender === gender);
